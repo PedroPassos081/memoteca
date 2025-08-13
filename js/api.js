408464f88c04e42a9c602595899eb417320c6f5e
Pedro Passos <pedro.passos081@gmail.com>
2025-08-12 22:58:55 -0300

    Adicionando dados com o POST

--- js/api.js
+++ js/api.js
@@ -8,6 +8,21 @@ const api = {
       throw Error;
     }
   },
+  async salvarPensamento() {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos", {
+        method: post,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return await response.json();
+    } catch {
+      alert("Erro ao salvar pensamentos");
+      throw Error;
+    }
+  },
 };
 
 export default api;
